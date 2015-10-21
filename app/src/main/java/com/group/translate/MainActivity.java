a5f0f891ee78a77245fee7a171a9758eaa876f3a
Anders Frid <anders.frid85@gmail.com>
2015-10-21 14:43:22 +0200

    suger

--- app/src/main/java/com/group/translate/MainActivity.java
+++ app/src/main/java/com/group/translate/MainActivity.java
@@ -3,6 +3,7 @@ package com.group.translate;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Andreas", "andreas suger");
     }
 
     @Override
