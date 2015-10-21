fb43f053bba1be29d6a878cba324e8483b37f55d
Joakim Gustavsson Sköld <joakim_naw@hotmail.com>
2015-10-21 15:07:04 +0200

    hello gais

--- app/src/main/java/com/group/translate/MainActivity.java
+++ app/src/main/java/com/group/translate/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Andreas", "fuck you guys!");
+        Log.d("Peter Pan", "I'M LIKE A BIIIRD, I WANNA FLY AWAAAAY");
     }
 
     @Override
