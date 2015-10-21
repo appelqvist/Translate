e40ad95a2de092857554c0280750cd8244992b15
David <davve52@hotmail.com>
2015-10-21 14:49:40 +0200

    HEEEJ

--- app/src/main/java/com/group/translate/MainActivity.java
+++ app/src/main/java/com/group/translate/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Andreas", "andreas suger");
+        Log.d("Andreas", "andreas suger riktigt mycket");
     }
 
     @Override
