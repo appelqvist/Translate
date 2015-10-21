91d2d6e4a2ad70d10dec2feefffa547b1a098aed
David <davve52@hotmail.com>
2015-10-21 15:22:47 +0200

    Lagt till variabler och gjort en knapplyssnare

--- app/src/main/java/com/group/translate/MainFragment.java
+++ app/src/main/java/com/group/translate/MainFragment.java
@@ -6,12 +6,20 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
+    private Button btnSpeak;
+    private EditText etInput;
+    private Spinner spCategory, spLanguage;
+    private ListView lvPhrases;
 
 
     public MainFragment() {
@@ -22,9 +30,31 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        init(view);
+        listener();
+        return view;
+    }
+
+    private void listener() {
+        ButtonListener bl = new ButtonListener();
+        btnSpeak.setOnClickListener(bl);
+
+    }
+
+    private void init(View view) {
+        btnSpeak = (Button)view.findViewById(R.id.btnSpeak);
+        etInput = (EditText)view.findViewById(R.id.etInput);
+        spCategory = (Spinner)view.findViewById(R.id.spCategory);
+        spLanguage = (Spinner)view.findViewById(R.id.spLanguage);
+        lvPhrases = (ListView)view.findViewById(R.id.lvPhrase);
     }
 
 
+    private class ButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
 }
