42e0205672eb7bcf35e3538eae41ab7c29aa6559
Andréas Appelqvist <andreas.appelqvist@gmail.com>
2015-10-22 10:21:08 +0200

    Hej David jag har gjort sakerna vi snackade om

--- app/src/main/java/com/group/translate/MainActivity.java
+++ app/src/main/java/com/group/translate/MainActivity.java
@@ -8,12 +8,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+    private Controller controller;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Peter Pan", "I'M LIKE A BIIIRD, I WANNA FLY AWAAAAY");
+        controller = new Controller(this);
     }
 
     @Override
