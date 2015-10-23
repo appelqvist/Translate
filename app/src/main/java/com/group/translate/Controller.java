54120cc9a6ddf336db1b0487af70db886163551c
Andréas Appelqvist <andreas.appelqvist@gmail.com>
2015-10-23 11:57:47 +0200

    Lagt till så att Listview fungerar Bara att anpassa translator till metoderna

--- app/src/main/java/com/group/translate/Controller.java
+++ app/src/main/java/com/group/translate/Controller.java
@@ -1,5 +1,7 @@
 package com.group.translate;
 
+import android.util.Log;
+
 import java.io.IOException;
 import java.net.URL;
 
@@ -9,6 +11,7 @@ import java.net.URL;
 
 public class Controller {
 
+    private String TAG = "Controller";
     private MainActivity activity;
     private MainFragment mainFragment;
     private Speaker speaker;
@@ -23,6 +26,27 @@ public class Controller {
             this.activity.setFragment(mainFragment, "mainFragment", false);
             this.mainFragment.setController(this);
         }
+    }
+
+    public String translate(String inLang, String outLang, String phrase){
+        //Översätter ordet
+        //Returnerar de översatta ordet.
+        return TAG+".translate";
+    }
+
+    public void translateAndSpeak(String inLang, String outLang, String swePhrase){
+        Log.d(TAG, "InputLang: "+inLang);
+        Log.d(TAG,"OutputLang: "+outLang);
+        Log.d(TAG,"SwePhrase: "+swePhrase);
+
+
+        //Göra en översättning på ordet swePhrase
+        //Från Svenksa till Angivet språk
+        //Tar det översatta ordet som använder playText(outLang, nyaOrdet)
+
+
+        //Ska tas bort sen:
+        playText(outLang, swePhrase);
     }
 
     public void playText(String lang, String phrase){
--- app/src/main/java/com/group/translate/MainFragment.java
+++ app/src/main/java/com/group/translate/MainFragment.java
@@ -7,11 +7,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
 /**
@@ -19,11 +21,14 @@ import android.widget.Spinner;
  */
 public class MainFragment extends Fragment {
 
-    private Button btnSpeak;
+    private String TAG = "MainFragment";
+    private Button btnSpeak, btnTranslate;
     private EditText etInput;
-    private Spinner spCategory, spLanguage;
+    private Spinner spOutLanguage, spInLanguage;
+    private TextView tvTextTranslated;
     private ListView lvPhrases;
     private Controller controller;
+    private String[] phrases;
 
     public MainFragment() {
         // Required empty public constructor
@@ -41,42 +46,63 @@ public class MainFragment extends Fragment {
     }
 
     private void setListViewAdapter() {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,
-                getActivity().getResources().getStringArray(R.array.Swe_Phrases));
+        phrases = getActivity().getResources().getStringArray(R.array.Swe_Phrases);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, phrases);
         lvPhrases.setAdapter(adapter);
     }
 
     private void listener() {
         ButtonListener bl = new ButtonListener();
         btnSpeak.setOnClickListener(bl);
+        btnTranslate.setOnClickListener(bl);
+
+
+        //ListView Listener
+        lvPhrases.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "Klickade på: " + phrases[position]);
+                controller.translateAndSpeak("Svenska", spOutLanguage.getSelectedItem().toString(), phrases[position]);
+            }
+        });
 
     }
-
 
     private void init(View view) {
-        btnSpeak = (Button)view.findViewById(R.id.btnSpeak);
-        etInput = (EditText)view.findViewById(R.id.etInput);
-        //spCategory = (Spinner)view.findViewById(R.id.spCategory);
-        spLanguage = (Spinner)view.findViewById(R.id.spLanguage);
-        lvPhrases = (ListView)view.findViewById(R.id.lvPhrase);
+        btnSpeak = (Button) view.findViewById(R.id.btnSpeak);
+        btnTranslate = (Button)view.findViewById(R.id.btnTranslate);
+        etInput = (EditText) view.findViewById(R.id.etInput);
+        tvTextTranslated = (TextView)view.findViewById(R.id.tvTextTranslated);
+        spOutLanguage = (Spinner) view.findViewById(R.id.spLangOut);
+        spInLanguage = (Spinner) view.findViewById(R.id.spLanguage);
+        lvPhrases = (ListView) view.findViewById(R.id.lvPhrase);
     }
 
-    private void setSpinnerAdapter(){
+    private void setSpinnerAdapter() {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,
                 getActivity().getResources().getStringArray(R.array.Lang_Array));
-        spLanguage.setAdapter(adapter);
+        spInLanguage.setAdapter(adapter);
+        spOutLanguage.setAdapter(adapter);
+    }
+
+    private void setTextTranslated(String str){
+        tvTextTranslated.setText(str);
     }
 
     public void setController(Controller controller) {
         this.controller = controller;
     }
 
-
     private class ButtonListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            controller.playText((String)spLanguage.getSelectedItem().toString(), etInput.getText().toString());
-            Log.d("", "Klickar ");
+            if (v.getId() == R.id.btnSpeak) {
+                controller.translateAndSpeak(spInLanguage.getSelectedItem().toString(),spOutLanguage.getSelectedItem().toString(), etInput.getText().toString());
+                Log.d(TAG, "Klickar ");
+            } else if (v.getId() == R.id.btnTranslate) {
+                Log.d(TAG,"Click");
+                String newText = controller.translate(spInLanguage.getSelectedItem().toString(), spOutLanguage.getSelectedItem().toString(),etInput.getText().toString());
+                MainFragment.this.setTextTranslated(newText);
+            }
         }
     }
 }
